dbc1211e12e239cbab8c5843c151a17624b22b6c
Leo Kamwathi <Leo.Kamwathi@gmail.com>
2019-11-07 02:56:20 +0300

    Finished GET/Feed Endpoint Test

---
+++ server/model/posts.js
@@ -0,0 +1,32 @@
+// Handels Articles and gifs.
+// T'status' : 'success' ,
+
+const posts = [
+    {
+'data': [
+    {
+        'id' : 1,
+        'createdOn' : '07-05-2019',
+        'title' : 'Happy Sunday',
+        'post' : 'Today will be a fantastic day.',
+        'isGif': 'false',
+    'authorId' : 2,
+}, {
+        'id' : 2,
+        'createdOn': '07-05-2019',
+        'title' : 'Cats',
+        'post': 'https://picsum.photos/300',
+        'isGif':'true',
+    'authorId' : 3,
+}, {
+        'id' : 3,
+        'createdOn': '07-05-2019',
+        'title' : 'Always Keep tring.',
+        'post' : 'I am alive today because I never gaveup yestarday.',
+        'isGif': 'false',
+    'authorId' : 3,
+},
+]
+}];
+
+module.exports = posts;
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,17 +1,23 @@
 const Express = require('express');
-
+const data = require('../model/posts');
 
 const Router = Express.Router();
 
 Router.get('/v1/feed', (req, res, next) => {
-    res.status(200).json({ "message": "Articles" });
+    res.status(200).json(data);
     console.log('Successfuly retrived all articles feed');
     next();
 });
 
-Router.get('/v1/article/:id', (req, res, next) => {
-    res.status(200).json({ "message": "Article 1" });
+Router.get('/v1/articles/:id', (req, res, next) => {
+    res.status(200).json(data[0].data[req.params.id]);
     console.log('Successfuly retrived article');
+    next();
+});
+
+Router.get('/v1/gifs/:id', (req, res, next) => {
+    res.status(200).json(data[0].data[req.params.id]);
+    console.log('Successfuly retrived gif');
     next();
 });
 
