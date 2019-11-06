5b46c13f652fe273e4ccce13ed8b632bc8aea891
Leo Kamwathi <Leo.Kamwathi@gmail.com>
2019-11-07 00:59:28 +0300

    Fix #2

--- server/app.js
+++ server/app.js
@@ -5,8 +5,10 @@ const apiRoutes = require('./routes/api');
 
 
 
+
 const app = express();
 
+app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
